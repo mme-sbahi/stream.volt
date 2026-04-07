f461b386bcf34da4a148e9224cd2c292e692332f
mmes-bahi <mmesbahi@student.1337.ma>
2026-04-07 18:02:13 +0100

    Initial commit

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -26,8 +26,8 @@ export default function Hero() {
       <div className="absolute inset-0 z-0 overflow-hidden">
         <div className="absolute inset-0 bg-black">
           <iframe
-            className="absolute top-1/2 left-1/2 w-[300vw] h-[300vh] min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-            src="https://www.youtube.com/embed/s9gSuKaKcqM?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&rel=0&playlist=s9gSuKaKcqM&playsinline=1&disablekb=1&fs=0&modestbranding=1&iv_load_policy=3&branding=0&start=10"
+            className="absolute top-1/2 left-1/2 w-full h-full min-w-[177.78vh] min-h-[56.25vw] -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+            src="https://www.youtube.com/embed/s9gSuKaKcqM?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&rel=0&playlist=s9gSuKaKcqM&playsinline=1&disablekb=1&fs=0&modestbranding=1&iv_load_policy=3&branding=0&vq=hd1080&wmode=transparent"
             allow="autoplay; encrypted-media"
             loading="eager"
             title=""
